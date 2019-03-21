8e3aace2319505f95bf854d2da956b0866ef3b31
Reece Spragg <reece.spragg@gmail.com>
2019-03-21 18:20:32 +1300

    Completed Java Implementation. -Now has skeleton functions that can be used change the screens via the Navigation Bar. -Added button action listener for when we want to take a photo.

--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/MainActivity.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/MainActivity.java
@@ -1,10 +1,13 @@
 package receiptstacker.pp159333.com.receiptstacker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -19,16 +22,12 @@ public class  MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_scan:
-                    mTextMessage.setText(R.string.title_scan);
                     return true;
                 case R.id.navigation_browse:
-                    mTextMessage.setText(R.string.title_browse);
                     return true;
                 case R.id.navigation_gstcal:
-                    mTextMessage.setText(R.string.title_gstcalc);
                     return true;
                 case R.id.navigation_options:
-                    mTextMessage.setText(R.string.title_options);
                     return true;
             }
             return false;
@@ -37,14 +36,28 @@ public class  MainActivity extends AppCompatActivity {
         }
     };
 
+    private ImageView.OnClickListener mOnClickListener = new ImageView.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //This is where the camera will take a photo
+            System.out.print("Camera Click");
+            ImageView camera = findViewById(R.id.imageView_Camera);
+            camera.setBackgroundColor(Color.BLACK);
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ImageView cameraShutter = findViewById(R.id.imageViewCameraShutter);
+        cameraShutter.setOnClickListener(mOnClickListener);
+
     }
 
 }
