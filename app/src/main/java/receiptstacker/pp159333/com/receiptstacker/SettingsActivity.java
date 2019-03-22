8ffb47ea89ca9902a8025fbfa5cb423fd1fe7d57
Brennan Grieve <brennangrieve45@gmail.com>
2019-03-22 19:47:50 +1300

    Implemented Setting Icons and created Silver Theme data
    Changelog;
    
    -Capture Options and Personalization Options now have icons
    
    -Silver Theme Color data added
    
    -Silver Theme Added, but not yet implemented.

--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/SettingsActivity.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/SettingsActivity.java
@@ -12,8 +12,6 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -245,7 +243,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_color_scheme);
+            addPreferencesFromResource(R.xml.pref_personalize);
             setHasOptionsMenu(true);
 
 
