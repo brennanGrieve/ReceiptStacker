c8d8b6a6020b356b920fcef1d9078acc4f8917fb
James Waddell <jamesgwaddell@gmail.com>
2019-03-29 15:49:08 +1300

    Notes: -Added a search bar and button. -Added an event listener for the search button. -Changed a few colours.

--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/BrowseActivity.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/BrowseActivity.java
@@ -3,16 +3,26 @@ package receiptstacker.pp159333.com.receiptstacker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BrowseActivity extends AppCompatActivity {
 
+    Button btn1;
     static String TAG = "BrowseActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "Running");
         setContentView(R.layout.activity_browse);
-
+        btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add the search stuff here.
+            }
+        });
     }
 
 
