11fbd120a3f86d0b83e52637cd3874ceea2d8269
Brennan Grieve <brennangrieve45@gmail.com>
2019-04-15 22:03:17 +1200

    Removed unused package imports

--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/PhotoActivity.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/PhotoActivity.java
@@ -11,12 +11,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
 
 public class PhotoActivity extends AppCompatActivity {
 
--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/ScanFragment.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/ScanFragment.java
@@ -1,11 +1,7 @@
 package receiptstacker.pp159333.com.receiptstacker;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
