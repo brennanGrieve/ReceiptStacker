0905b2914ccc339bfcd484678a7a54a44686e82e
Brennan Grieve <brennangrieve45@gmail.com>
2019-04-30 01:29:02 +1200

    Beginning of Single Capture Implementation
    Implemented Camera Capture
    
    Removed many unused Dependencies

--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/ScanFragment.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/ScanFragment.java
@@ -1,16 +1,17 @@
 package receiptstacker.pp159333.com.receiptstacker;
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -24,7 +25,6 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 
 public class ScanFragment extends Fragment {
@@ -32,7 +32,7 @@ public class ScanFragment extends Fragment {
     static Context appContext;
     final int RequestCameraPermissionID = 1001;
     TextView textView;
-
+    static CameraSource cameraSource;
 
     public static ScanFragment newInstance(Context context) {
         ScanFragment fragment = new ScanFragment();
@@ -62,6 +62,31 @@ public class ScanFragment extends Fragment {
 
     }
 
+    CameraSource.PictureCallback pCallBack = new CameraSource.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] bytes) {
+            //handle loading the image into the receipt here
+            Bitmap pic = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+            ImageView image = new ImageView(getActivity());
+            image.setImageBitmap(pic);
+            AlertDialog.Builder builder =
+                    new AlertDialog.Builder(getActivity()).
+                            setMessage("Message above the image").
+                            setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).
+                            setView(image);
+            builder.create().show();
+        }
+    };
+
+    private void captureData(){
+        cameraSource.takePicture(null, pCallBack);
+    }
+
     private ImageView.OnClickListener mOnClickListener = new ImageView.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -69,14 +94,13 @@ public class ScanFragment extends Fragment {
             System.out.print("Camera Click");
             ImageView camera = getView().findViewById(R.id.surfaceView_Camera);
             camera.setBackgroundColor(Color.BLACK);*/
+            captureData();
         }
     };
 
     public void getCamera(View v) {
         cameraView = v.findViewById(R.id.surfaceView_Camera);
         textView = v.findViewById(R.id.textView);
-        final CameraSource cameraSource;
-
         TextRecognizer textRecognizer = new TextRecognizer.Builder(appContext).build();
         if (!textRecognizer.isOperational()) {
             Log.w("MainActivity", "Dependencies are not available");
