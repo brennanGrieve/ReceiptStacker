38c8ffe6fc9a7f4b199f53473b1e2949dd6e734c
Brennan Grieve <brennangrieve45@gmail.com>
2019-05-28 02:11:35 +1200

    Pruning
    Settings Activity Pruning

--- app/src/main/java/receiptstacker/pp159333/com/receiptstacker/SettingsActivity.java
+++ app/src/main/java/receiptstacker/pp159333/com/receiptstacker/SettingsActivity.java
@@ -30,68 +30,15 @@ import java.util.List;
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
 
-
+    /**
+     * Event Listener to cause a recreation when the Theme setting is changed, to make effects immediate.
+     */
     private SharedPreferences.OnSharedPreferenceChangeListener sUpdateThemeView = new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
            recreate();
         }
     };
-
-
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-//    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-//        @Override
-//        public boolean onPreferenceChange(Preference preference, Object value) {
-//
-//            String stringValue = value.toString();
-//
-//            if (preference instanceof ListPreference) {
-//                // For list preferences, look up the correct display value in
-//                // the preference's 'entries' list.
-//                ListPreference listPreference = (ListPreference) preference;
-//                int index = listPreference.findIndexOfValue(stringValue);
-//
-//                // Set the summary to reflect the new value.
-//                preference.setSummary(
-//                        index >= 0
-//                                ? listPreference.getEntries()[index]
-//                                : null);
-//
-//            } else if (preference instanceof RingtonePreference) {
-//                // For ringtone preferences, look up the correct display value
-//                // using RingtoneManager.
-//                if (TextUtils.isEmpty(stringValue)) {
-//                    // Empty values correspond to 'silent' (no ringtone).
-//                    preference.setSummary(R.string.pref_ringtone_silent);
-//
-//                } else {
-//                    Ringtone ringtone = RingtoneManager.getRingtone(
-//                            preference.getContext(), Uri.parse(stringValue));
-//
-//                    if (ringtone == null) {
-//                        // Clear the summary if there was a lookup error.
-//                        preference.setSummary(null);
-//                    } else {
-//                        // Set the summary to reflect the new ringtone display
-//                        // name.
-//                        String name = ringtone.getTitle(preference.getContext());
-//                        preference.setSummary(name);
-//                    }
-//                }
-//
-//            } else {
-//                // For all other preferences, set the summary to the value's
-//                // simple string representation.
-//                preference.setSummary(stringValue);
-//            }
-//
-//            return true;
-//        }
-//    };
 
 
     /**
@@ -103,28 +50,10 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
     }
 
-
-//    /**
-//     * Binds a preference's summary to its value. More specifically, when the
-//     * preference's value is changed, its summary (line of text below the
-//     * preference title) is updated to reflect the value. The summary is also
-//     * immediately updated upon calling this method. The exact display format is
-//     * dependent on the type of preference.
-//     *
-//     * @see #sBindPreferenceSummaryToValueListener
-//     */
-//    private static void bindPreferenceSummaryToValue(Preference preference) {
-//        // Set the listener to watch for value changes.
-//        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-//
-//        // Trigger the listener immediately with the preference's
-//        // current value.
-//        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-//                PreferenceManager
-//                        .getDefaultSharedPreferences(preference.getContext())
-//                        .getString(preference.getKey(), ""));
-//
-//    }
+    /**
+     * Method fired on creation of Settings Activity
+     * @param savedInstanceState Saved state of the last known Instance of the Activity.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -176,6 +105,10 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    /**
+     * Method fired when a Menu Item is selected
+     */
+
     @Override
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         int id = item.getItemId();
@@ -217,7 +150,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     }
 
     /**
-     * This fragment shows general preferences only. It is used when the
+     * This fragment shows Scan Settings. It is used when the
      * activity is showing a two-pane settings UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -231,6 +164,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
         }
 
+        /**
+         * Method fired when an options item is selected.
+         * @param item The Selected Item.
+         * @return Superclass handling of Selected item.
+         */
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
@@ -242,6 +180,10 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    /**
+     * This Placeholder settings fragment is not currently utilized. It is kept in case it is needed. If unused, will be removed before release.
+     */
+
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class PlaceholderPreferenceFragment extends PreferenceFragment {
         @Override
@@ -249,13 +191,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_placeholder_settings);
             setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            //bindPreferenceSummaryToValue(findPreference("example_text"));
-            //bindPreferenceSummaryToValue(findPreference("example_list"));
         }
 
         @Override
@@ -271,7 +206,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
 
     /**
-     * This fragment shows data and sync preferences only. It is used when the
+     * This fragment shows personalization preferences. It is used when the
      * activity is showing a two-pane settings UI.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -281,12 +216,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_personalize);
             setHasOptionsMenu(true);
-
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
         }
 
         @Override
